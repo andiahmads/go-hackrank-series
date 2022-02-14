73aef288e203fabff5b6798fa2ef208ed9af18f2
andiahmads <andi.fivesco@gmail.com>
2022-02-14 15:59:39 +0700

    addding information

--- sum_total_index/impt_test.go
+++ sum_total_index/impt_test.go
@@ -8,16 +8,20 @@ import (
 
 func SumTotal(list []int) map[int]int {
 
-	//sort array asc
-
+	//maping object
 	var countSameIndex = make(map[int]int)
+
 	for _, v := range list {
 
+		// varibel dibawah untuk cek total index yg sama
+		// yg akan mengembalikan nilai true & false
 		_, exist := countSameIndex[v]
+
+		//jika elemet sama dengan true +1
 		if exist {
 			countSameIndex[v] += 1
 		} else {
-			countSameIndex[v] = 1
+			countSameIndex[v] = 1 // jika false ttp 1
 		}
 	}
 
@@ -28,12 +32,14 @@ func SumTotal(list []int) map[int]int {
 func TestTestingSum(t *testing.T) {
 	var arr = []int{700, 400, 200, 800, 700, 100, 200, 100, 300}
 
+	//sorting array
 	sort.Slice(arr, func(i, j int) bool {
 		return arr[i] > arr[j]
 	})
 
 	count := SumTotal(arr)
 
+	//cetak array
 	for key, val := range count {
 		fmt.Println(key, "berjumlah = ", val)
 	}
