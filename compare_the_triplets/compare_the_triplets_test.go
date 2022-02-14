44a978c3b302afc568abc5cc11264993c78a24b1
andiahmads <andi.fivesco@gmail.com>
2022-02-14 16:11:13 +0700

    refactoring coding

--- compare_the_triplets/compare_the_triplets_test.go
+++ compare_the_triplets/compare_the_triplets_test.go
@@ -5,29 +5,28 @@ import (
 	"testing"
 )
 
-func CompareTheTriplets(a, b [3]int) (int, int) {
+func CompareArrayScore(a, b []int) (int, int) {
 
-	var cok, kocok int
+	var arr1 int
+	var arr2 int
+
 	for i := 0; i < len(a); i++ {
 		if a[i] > b[i] {
-			cok += 1
-		} else if a[i] < b[i] {
-			kocok += 1
+			arr1 += 1
+		} else if b[i] > a[i] {
+			arr2 += 1
 		}
 	}
 
-	return cok, kocok
+	return arr1, arr2
 
 }
 
-func TestCompareTheTriplets(t *testing.T) {
+func TestCompareArrayScores(t *testing.T) {
+	arr1 := []int{1, 5, 7, 1, 5, 7}
+	arr2 := []int{2, 2, 3, 1, 5, 7}
 
-	var andi = [3]int{3, 5, 3}
-	var ahmad = [3]int{1, 4, 5}
+	res, res2 := CompareArrayScore(arr1, arr2)
 
-	a, b := CompareTheTriplets(andi, ahmad)
-
-	fmt.Println("andi = ", a, "ahmad", b)
-
-	// assert.Equal(t, a, b, result)
+	fmt.Println("arr1 = ", res, "arr2 = ", res2)
 }
