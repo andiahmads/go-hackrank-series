a35496419aff7667e82c861c3bfe6ae5dbf21bbb
andi.fivesco@gmail.com <andi.fivesco@gmail.com>
2022-03-04 10:10:48 +0700

    min max index array

---
+++ minMaxIndexArray/impl_test.go
@@ -0,0 +1,31 @@
+package minmaxindexarray
+
+import (
+	"fmt"
+	"testing"
+)
+
+func minMaxIndexArray(arr []int) (int, int) {
+
+	var min, max int
+	for i, val := range arr {
+		switch {
+		case i == 0:
+			min, max = val, val
+		case i > max:
+			max = val
+		case i < min:
+			min = val
+		}
+	}
+	return min, max
+}
+
+func TestMinMaxIndexArray(t *testing.T) {
+	var arr = []int{-1, 1, 2, 3, 4, 5, 21}
+
+	min, max := minMaxIndexArray(arr)
+
+	fmt.Println(min, max)
+
+}
