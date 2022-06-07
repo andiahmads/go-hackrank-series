ffde5b21be6524bb77d7f2dd5a08cd0d7fbdba44
andiahmads <andi.fivesco@gmail.com>
2022-06-07 19:02:09 +0700

    blue bird

--- bluebird/impl_test.go
+++ bluebird/impl_test.go
@@ -1,8 +1,28 @@
 package bluebird
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestBlueBird(t *testing.T) {}
+func TestBlueBird(t *testing.T) {
+	arr := []int{8, 5, 9, 10, 2, 90}
+	res := Bluebird(arr)
+	fmt.Println(res)
 
+}
 
+func Bluebird(arr []int) int {
 
+	terang := 0
+	gedung_tarang := 0
+	for _, v := range arr {
+
+		if v > terang {
+			terang = v
+			gedung_tarang++
+		}
+
+	}
+	return gedung_tarang
+}
